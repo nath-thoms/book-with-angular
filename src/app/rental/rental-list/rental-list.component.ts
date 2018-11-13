5293a17f6a18b26223178d66008fe6ccd8bb36d8
nath <nath.chadwick@gmail.com>
2018-11-13 15:24:45 +0000

    implemented rental service

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RentalService } from '../shared/rental.service';
 
 @Component({
   selector: 'bwm-rental-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RentalListComponent implements OnInit {
 
-  constructor() { }
+  rentals: any[] = [];
+
+  constructor(private rentalService: RentalService ) { }
 
   ngOnInit() {
+    this.rentals = this.rentalService.getRentals();
   }
 
 }
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,8 +1,10 @@
 import {NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalComponent } from './rental.component';
 
+import { RentalService } from './shared/rental.service';
 
 @NgModule ({
     declarations: [
@@ -10,6 +12,7 @@ import { RentalComponent } from './rental.component';
         RentalListItemComponent,
         RentalComponent
     ],
-    imports: []
+    imports: [CommonModule],
+    providers: [RentalService]
 })
 export class RentalModule {}
